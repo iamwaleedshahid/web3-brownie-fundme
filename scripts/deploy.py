4501b944277a67a3ba6108f1d1a8d5b504c7e28d
dev-waleed <hiwaleed@outlook.com>
2021-09-29 13:05:43 +0500

    Making Project Chain Agnostic

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,17 +1,27 @@
 import imp
-from brownie import FundMe, network
+from brownie import FundMe, network, config, MockV3Aggregator
 from scripts.helpful_scripts import get_account
+
 
 def deploy_fund_me():
     account = get_account()
 
     if network.show_active() != "development":
-        price_feed_address = "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e"
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        print(f"The active network is {network.show_active()}")
+        print("Deploying Mocks...")
+        mock_aggregator = MockV3Aggregator.deploy(18, 2000000000000000000, {"from": account})
+        price_feed_address = mock_aggregator.address
+        print("Mocks Deployed!")
+
     fund_me = FundMe.deploy(
-        '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
-        {"from":account},
-        publish_source=True,
-        )
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
 
 
 def main():
